3b6844f49d77c6cf7b6babff5459ada32b5dba80
plfelter <hurdle0.foggier@icloud.com>
2025-05-08 16:58:56 +0200

    Flask start boilerplate

---
+++ tony/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
+
+persistent_path: Path = Path(__file__).resolve().parent
+db_path = persistent_path / "sqlite.db"
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{db_path}'
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy()
+
+from tony import views
+from tony import models
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from tony import app
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+# WSGI entry point
+application = app
