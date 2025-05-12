9091f26edbf1ec91dc95f7f81a9d1ce1f9ad34a2
plfelter <hurdle0.foggier@icloud.com>
2025-05-12 22:55:00 +0200

    Script to anonymise the data

--- scripts/anonymise-contrib-json.py
+++ scripts/anonymise-contrib-json.py
@@ -7,5 +7,19 @@ for item in data:
     if item['user'] != 'Anonyme':
         item['user'] = 'Anonymisé'
 
-with open('../verbatims/contributions-anonymisees.json', 'w+', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+import pandas as pd
+pd.DataFrame.from_dict(data).to_csv('../verbatims/contributions-anonymisees.csv', index=False)
+
+# Write to JSON file
+# with open('../verbatims/contributions-anonymisees.json', 'w+', encoding='utf-8') as f:
+#     json.dump(data, f, ensure_ascii=False, indent=4)
+
+# Write to JSONL file (one JSON object per line)
+# with open('../verbatims/contributions-anonymisees.jsonl', 'w+', encoding='utf-8') as f:
+#     if isinstance(data, list):
+#         # If the JSON is an array of objects
+#         for item in data:
+#             f.write(json.dumps(item, ensure_ascii=False) + '\n')
+#     else:
+#         # If the JSON is a single object
+#         f.write(json.dumps(data, ensure_ascii=False) + '\n')
